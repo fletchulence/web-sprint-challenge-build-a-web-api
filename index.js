94d3856cb7e079a38402f9aa0c55a2ade0d5ecc3
david fletcher <davidfletcheraaron@gmail.com>
2022-01-13 23:26:10 -0500

    changed fallback

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 
 const server = require('./api/server');
 
-const PORT = process.env.PORT || 9008;
+const PORT = process.env.PORT || 9000;
 
 server.listen(PORT, ()=>{
    console.log(`listening on port: ${PORT}`)
