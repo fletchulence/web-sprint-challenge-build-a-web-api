65fdbb068219fe67733343c0976e44613b6f5743
david fletcher <davidfletcheraaron@gmail.com>
2022-01-14 01:54:45 -0500

    actions check for completed in body

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -4,6 +4,7 @@ const Action = require('./actions-model')
 module.exports = {
    checkBody,
    idExists,
+   checkComplete,
 }
 
 function checkBody (req, res, next) {
@@ -26,5 +27,29 @@ async function idExists (req, res, next) {
       }
    } catch(err){
       next(err)
+   }
+}
+
+function checkComplete (req, res, next){
+   // const {id} = req.params
+   // const completed = Project.get( id ) 
+   if ( req.body.completed === undefined ){
+      next({ status: 400, message: 'please reply "YES/NO" to completed field'})
+   } else {
+      req.completed = req.body.completed
+      if ( 
+         req.body.completed === 'yes' || 
+         req.body.completed === 'YES' || 
+         req.body.completed === 'y' || 
+         req.body.completed === 'T' || 
+         req.body.completed === 'TRUE' || 
+         req.body.completed === 't' || 
+         req.body.completed === 'true' || 
+         req.body.completed === true || 
+         req.body.completed === 'Y' 
+         ){
+         req.completed = true
+      } 
+      next()
    }
 }
