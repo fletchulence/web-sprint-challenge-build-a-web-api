f0399c97a2873969d3d22efa6deb77ea9cf3bc0d
david fletcher <davidfletcheraaron@gmail.com>
2022-01-14 00:42:19 -0500

    formatted projects router

--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -9,42 +9,42 @@ const {
 } = require('./projects-middleware')
 
 // [GET] all projects
-router.get('/', async (req, res, next) =>{
-   try{
+router.get('/', async (req, res, next) => {
+   try {
       res.json(await Project.get())
-   } catch(err){
+   } catch (err) {
       next(err)
    }
 });
 
 // [GET] by id -- need midd for if id DNE
-router.get('/:id', idExists, async (req, res, next) =>{
+router.get('/:id', idExists, async (req, res, next) => {
    // const {id} = req.params
-   try{
+   try {
       res.json(await Project.get(req.id))
-   } catch(err){
+   } catch (err) {
       next(err)
    }
 });
 
 // [POST] 
-router.post('/', checkBody, async (req, res, next)=>{
-   const newProject = await Project.insert( req.body )
-   try{
-      res.json( newProject )
-   } catch(err){
+router.post('/', checkBody, async (req, res, next) => {
+   const newProject = await Project.insert(req.body)
+   try {
+      res.json(newProject)
+   } catch (err) {
       next(err)
    }
 });
 
 // [PUT] updating the post based on the id
-router.put('/:id', idExists, checkBody, checkComplete,  async (req, res, next)=>{
+router.put('/:id', idExists, checkBody, checkComplete, async (req, res, next) => {
    // const {id}
    let changes = { ...req.body, completed: req.body.completed };
-   const updateProject = await Project.update( req.id, changes )
+   const updateProject = await Project.update(req.id, changes)
    try {
-      res.json( updateProject )
-   } catch(err){
+      res.json(updateProject)
+   } catch (err) {
       next(err)
    }
 })
